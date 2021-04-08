8e058a94db616c98e1fc656e3416b4a86948a836
Wimmerer <wrkimmerer@outlook.com>
2021-04-08 14:10:06 -0400

    Devcontainer and Scalar Construction

--- src/scalar.jl
+++ src/scalar.jl
@@ -2,7 +2,18 @@ mutable struct GrB_Scalar{T} <: Abstract_GrB_Struct
     p::Ptr{Cvoid}
 end
 
-GrB_Scalar{T}() where {T} = GrB_Scalar{T}(C_NULL)
+function GrB_Scalar{T}() where {T}
+    GrB_Scalar{T}(C_NULL)
+    
 
 function GxB_Scalar_new(S::GrB_Scalar{T})
-    S_ptr pointer_from_objref(S)
+    S_ptr = pointer_from_objref(S)
+
+    checkinfo(
+        ccall(
+            dlsym(graphblas_lib, "GrB_Scalar_new"),
+            GrB_Info,
+            (Ptr{Cvoid}, Ptr{Cvoid}),
+
+        )
+    )
--- src/utils.jl
+++ src/utils.jl
@@ -23,6 +23,33 @@ function suffix(T::DataType)
     return "FP64"
 end
 
+grb_ptr(x::Abstract_GrB_Struct) = x.p
+
+function ptr_to_GrB_Type(T::DataType)
+    if T == Bool
+        return grb_ptr(BOOL)
+    elseif T == Int8
+        return grb_ptr(INT8)
+    elseif T == UInt8
+        return grb_ptr(UINT8)
+    elseif T == Int16
+        return grb_ptr(INT16)
+    elseif T == UInt16
+        return grb_ptr(UINT16)
+    elseif T == Int32
+        return grb_ptr(INT32)
+    elseif T == UInt32
+        return grb_ptr(UINT32)
+    elseif T == Int64
+        return grb_ptr(INT64)
+    elseif T == UInt64
+        return grb_ptr(UINT64)
+    elseif  T == Float32
+        return grb_ptr(FP32)
+    end
+    return grb_ptr(FP64)
+end
+
 function load_global(str)
     x =
     try
